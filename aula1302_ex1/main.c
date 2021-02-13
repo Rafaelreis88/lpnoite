6cf0b24dcfb75ff2c23831d61b1cecb4fc5df821
maromo71m <professormoraes@gmail.com>
2021-02-13 11:34:22 -0300

    Primeira Aula

---
+++ aula1302_ex1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 13;
+    float b = 15.97;
+    printf("\tValor de A: %d \n", a);
+    printf("\tValor de B: %f \n", b);
+
+    return 0;
+}
---
+++ aula1302_ex2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+dsasdas jkasjdlk jasddkj asl lj asldjasl
+sadasjd lkasdl jasdlkjljads
+lad kljlasdljalkdsads
+*/
+int main()
+{
+    //escrever um comentario
+    printf("Hoje esta um \"LINDO\" dia \n");
+    printf("O \\n serve para pular uma linha");
+    return 0;
+}
